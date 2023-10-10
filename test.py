c276bf3c0b8e9c8656cf30f3e139c78a574a745e
Andrew de Quincey <adq@lidskialf.net>
2023-10-10 21:59:01 +0100

    checkin

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
 import aioble
 import asyncio
-import machine
-from machine import I2C
-import network
-import time
-import rp2
-import json
-from umqtt.simple import MQTTClient
-import secrets
+import cfgsecrets
 import mqtt_async
+import binascii
 
-LIGHT_SENSOR_I2C_ADDRESS = 0x23
-LIGHT_SENSOR_PULSE_THRESHOLD = 40
-WIFI_SSID = secrets.WIFI_SSID
-WIFI_PASSWORD = secrets.WIFI_PASSWORD
-MQTT_HOST = secrets.MQTT_HOST
+
+FAN_MAC = '58:2b:db:00:2e:68'
+FAN_PIN = '09135336'
+FAN_MAX_RPM = 2400
+
+SENSOR_MAC = 'A4:C1:38:AA:AF:F3'
+
 MQTT_STATE_TOPIC = 'homeassistant/sensor/picopower/state'
 MQTT_CONFIG_TOPIC = 'homeassistant/sensor/picopower/config'
-MQTT_CLIENT_ID = 'picopower'
+MQTT_CLIENT_ID = 'picoshower'
 
 
 # _IRQ_SCAN_RESULT = 5
@@ -30,12 +26,20 @@ MQTT_CLIENT_ID = 'picopower'
 #         print(address, binascii.hexlify(adv_data))
 #         # devices[address] = rssi
 
-async def yyy():
-    device = aioble.Device(0, '')  # 0 => ADDR_PUBLIC
-    connection = await device.connect()
+async def fan():
+    while True:
+        try:
+            print("MOOOO")
+            device = aioble.Device(0, FAN_MAC)  # 0 => ADDR_PUBLIC
+            connection = await device.connect(0)
+            async with connection:
+                print("FAN")
+        except Exception as ex:
+            print("FANFAIL")
+            print(type(ex))
+            print(ex)
+            await asyncio.sleep(5)
 
-    async with connection:
-        pass
         # try:
         #     temp_service = await connection.service(_ENV_SENSE_UUID)
         #     temp_characteristic = await temp_service.characteristic(_ENV_SENSE_TEMP_UUID)
@@ -49,12 +53,47 @@ async def yyy():
         #     await asyncio.sleep_ms(1000)
 
 
-async def xxx():
-    async with aioble.scan(0) as scanner:
-        async for result in scanner:
-            print(result, result.name(), result.rssi, list(result.services()))
+async def sensor():
+    while True:
+        try:
+            print("SCANNER")
+            async with aioble.scan(0) as scanner:
+                async for result in scanner:
+                    print("HEY", binascii.hexlify(result.device.addr, ':'))
+                    if result.device.addr == SENSOR_MAC:
+                        print("SENSOR", result, result.name(), result.rssi, list(result.services()))
+            await asyncio.sleep(1)
+        except Exception as ex:
+            print("SENSORFAIL")
+            print(ex)
+            await asyncio.sleep(5)
 
-    print("hi")
+
+def msg_callback(topic, msg, retained, qos):
+    pass
+
+
+async def conn_callback(client):
+    pass
+    # await client.subscribe(TOPIC, 1)
+
+
+async def mqtt():
+    mqtt_async.config['ssid'] = cfgsecrets.WIFI_SSID
+    mqtt_async.config['wifi_pw'] = cfgsecrets.WIFI_PASSWORD
+    mqtt_async.config['server'] = cfgsecrets.MQTT_HOST
+    mqtt_async.config['subs_cb'] = msg_callback
+    mqtt_async.config['connect_coro'] = conn_callback
+
+    mqc = mqtt_async.MQTTClient(mqtt_async.config)
+    await mqc.connect()
+    while True:
+        print("HELLO")
+        # print('publish', n)
+        # await client.publish(TOPIC, 'Hello World #{}!'.format(n), qos=1)
+        await asyncio.sleep(5)
+    # await mqc.subscribe(topic, qos)
+
 
 # if __name__ == "__main__":
 #     ble = bluetooth.BLE()
@@ -64,28 +103,28 @@ async def xxx():
 #     time.sleep(2)
 #     # ble.active('active')
 #     # with ble.gap_scan(30000, 50000, 50000) as scanner:
-#     #     for adv in scanner:
+#     #     for adv in scanner:cd de
 #     #         print(adv)
 
 async def main():
-    config = mqtt_async.config.copy()
-    config['ssid'] = WIFI_SSID
-    config['wifi_pw'] = WIFI_PASSWORD
-    config['server'] = MQTT_HOST
 
-    # Connect to WiFi
-    print("Connecting to WIFI")
-    rp2.country('GB')
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(WIFI_SSID, WIFI_PASSWORD)
-    while not wlan.isconnected():
-        print(wlan.status())
-        print('Waiting for connection...')
-        time.sleep(1)
-    print("Connected to WiFi")
+    # # Connect to WiFi
+    # print("Connecting to WIFI")
+    # rp2.country('GB')
+    # wlan = network.WLAN(network.STA_IF)
+    # wlan.active(True)
+    # wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+    # while not wlan.isconnected():
+    #     print(wlan.status())
+    #     print('Waiting for connection...')
+    #     time.sleep(1)
+    # print("Connected to WiFi")
 
-    await asyncio.gather(xxx(), yyy())
+    await asyncio.gather(
+        # fan(),mlknm;,
+                         sensor(),
+                        #  mqtt()
+                         )
 
 asyncio.run(main())
 print("HING")
