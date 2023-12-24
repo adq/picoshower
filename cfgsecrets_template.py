673259b4b1e924afbf476004dbf197ac69300327
Andrew de Quincey <adq@lidskialf.net>
2023-12-24 10:03:36 +0000

    template cfgsecrets

---
+++ cfgsecrets_template.py
@@ -0,0 +1,10 @@
+import binascii
+
+WIFI_SSID = "XXXX"
+WIFI_PASSWORD = "XXXX"
+MQTT_HOST = 'XXXX'
+
+FAN_MAC = binascii.unhexlify('deadbeefdead')
+FAN_PIN = '1234'
+
+SENSOR_MAC = binascii.unhexlify('deadbeefdead')
